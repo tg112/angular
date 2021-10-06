ee0e41318ea906a150362f5ad3692b5022ae3fe7
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-10-06 22:59:30 +0900

    done: moduleセクション

---
+++ comps/README.md
@@ -0,0 +1,6 @@
+moduleの作成  
+`ng generate module <MODULE_NAME> --routing`
+
+'Routing' flag tells angular to make this module ready for navigation
+
+新規作成したモジュールを使用する場合、そのモジュール内でコンポーネントをexportし、appでそのモジュールをインストールする
---
+++ comps/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { ModsModule } from './mods/mods.module';
+import { ViewsModule } from './views/views.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ModsModule,
+    ViewsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
