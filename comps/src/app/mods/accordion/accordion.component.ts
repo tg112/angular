f1c5a5e9147d0701f20ab22e8a7b798083128c99
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-10-16 23:07:15 +0900

    done: components semantic ui project

---
+++ comps/src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css']
+})
+export class AccordionComponent implements OnInit {
+  @Input() items: any = [];
+  openedItemIndex = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(i: number) {
+    if (i === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = i;
+    }
+  }
+
+}
--- comps/src/app/mods/mods-home/mods-home.component.ts
+++ comps/src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    {title: "Why is the sky blue?", content: "The sky is blue because it is"},
+    {title: "Why does an orange taste like?", content: "An orange tastes like an orange"},
+    {title: "What color is that cat?", content: "that cat is white"},
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
