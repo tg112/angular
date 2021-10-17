4204a889993edf84434324aef1ddaeb2384f3c31
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-10-17 14:35:55 +0900

    add: handle http section

---
+++ wsearch-http/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  term = "";
+  // 子から親に渡す
+  @Output() submitted = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
+}
